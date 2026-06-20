5877a373d057cccb08352415d19fa6b66fd7b50e
vedanta453-droid <vedanta453@gmail.com>
2026-06-20 11:06:47 +0530

    aaj maine loops padha

---
+++ FD10/loops.js
@@ -0,0 +1,7 @@
+//write a program to print of 2 
+sum=0
+for(i=1;i<=5;i++){
+sum=sum+i
+}
+console.log(sum);
+
---
+++ comparision/function.js
@@ -0,0 +1,28 @@
+// fuction with argument
+function multipication(a,b,c){
+    console.log("multipication of numbers:",a*,b*,c*);
+}
+
+multipication(2,4,6)// for loops  
+//  for(let i=1;i<=10;1++){
+//     console.log("Hello world");
+//     console.log(i)
+//  }
+    
+ //write a program to print table of 1 
+
+ for(let i=2; i <=20; i+= 2){
+    console.log(i);
+ }// for loops
+//  for(let i=1;i<=10;1++){
+//     console.log("Hello world");
+//     console.log(i)
+//  }
+    
+ //write a program to print table of 1 
+
+ for(let i=2; i <=20; i+= 2){
+    console.log(i);
+ }
+
+
